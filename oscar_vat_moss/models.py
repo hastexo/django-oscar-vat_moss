5cbbbb6905babf7a3995c3c08b48d7319e399437
Florian Haas <florian@hastexo.com>
2016-01-11 13:40:42 +0100

    Fix VATINField import

--- oscar_vat_moss/models.py
+++ oscar_vat_moss/models.py
@@ -1,10 +1,10 @@
 from oscar.apps.address import models
 from django.utils.translation import ugettext_lazy as _
 
-from fields import VATINField
+from . import fields
 
 class AbstractShippingAddress(models.AbstractShippingAddress):
-    vatin = VATINField(
+    vatin = fields.VATINField(
         _('VAT Identification Number (VATIN)'),
         blank=True,
         help_text=_('Required if you are associated with a business '
