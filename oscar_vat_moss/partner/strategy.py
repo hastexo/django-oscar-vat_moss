cbbd5201bfb4e4d4f566909f57ddb899b3878aef
Florian Haas <florian@hastexo.com>
2016-01-12 22:40:00 +0100

    Rename VATStrategy to DeferredVATStrategy

--- oscar_vat_moss/partner/strategy.py
+++ oscar_vat_moss/partner/strategy.py
@@ -3,9 +3,18 @@ from oscar.apps.partner import strategy
 
 class Selector(object):
     def strategy(self, request=None, user=None, **kwargs):
-        return VATStrategy(request)
+        return DeferredVATStrategy(request)
 
 
-class VATStrategy(strategy.UseFirstStockRecord, strategy.DeferredTax,
-                  strategy.StockRequired, strategy.Structured):
+class DeferredVATStrategy(strategy.UseFirstStockRecord, strategy.DeferredTax,
+                          strategy.StockRequired, strategy.Structured):
+    """Strategy that defers tax assessment until checkout.
+
+    With this strategy, your users always see a tax-free amount and
+    taxes are added to their invoice on checkout.
+
+    Requires that a CheckoutSessionMixin be added to your checkout
+    app, which then applies taxes once the user has given their
+    shipping address.
+    """
     pass
