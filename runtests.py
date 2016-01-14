10a962e9ad7a46816b6cee261aee4d1aae6c1e23
Florian Haas <florian@hastexo.com>
2016-01-14 15:43:45 +0100

    Don't generate HTML coverage report in runtests.py

--- runtests.py
+++ runtests.py
@@ -102,8 +102,6 @@ def run_tests(*test_args):
 
     if num_failures > 0:
         sys.exit(num_failures)
-    print("Generating HTML coverage report")
-    c.html_report()
 
 
 if __name__ == '__main__':
