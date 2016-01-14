7d0b9bf38b48957ba9cec24e326b360dc43cada2
Florian Haas <florian@hastexo.com>
2016-01-14 23:17:39 +0100

    Put test settings in separate file
    Create separate test_settings.py resembling a pristine Oscar project,
    rather than the crufty settings in runtest.py.
    
    Re-instate a couple of configuration settings:
    - Set NOSE_ARGS
    - Add Oscar 1.0 fixup

--- runtests.py
+++ runtests.py
@@ -1,94 +1,12 @@
 #!/usr/bin/env python
 import sys
+import os
 from coverage import coverage
 from optparse import OptionParser
 
-from django.conf import settings
-
-if not settings.configured:
-    extra_settings = {
-    }
-    # To specify integration settings (which include passwords, hence why they
-    # are not committed), create an integration.py module.
-    try:
-        from integration import *
-    except ImportError:
-        extra_settings.update({
-        })
-    else:
-        for key, value in list(locals().items()):
-            if key.startswith('VAT_MOSS'):
-                extra_settings[key] = value
-
-    from oscar.defaults import *
-    for key, value in list(locals().items()):
-        if key.startswith('OSCAR'):
-            extra_settings[key] = value
-    extra_settings['OSCAR_ALLOW_ANON_CHECKOUT'] = True
-
-    from oscar import get_core_apps, OSCAR_MAIN_TEMPLATE_DIR
-
-    settings.configure(
-        DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:', }, },
-        INSTALLED_APPS=[
-            'django.contrib.auth',
-            'django.contrib.admin',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.sites',
-            'django.contrib.flatpages',
-            'django.contrib.staticfiles',
-            'compressor',
-        ] + get_core_apps([
-            'oscar_vat_moss.address',
-            'oscar_vat_moss.checkout',
-            'oscar_vat_moss.partner',
-            'oscar_vat_moss.payment',
-            'oscar_vat_moss.order',
-        ]),
-        MIDDLEWARE_CLASSES=(
-            'django.middleware.common.CommonMiddleware',
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            'django.middleware.csrf.CsrfViewMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.messages.middleware.MessageMiddleware',
-            'oscar.apps.basket.middleware.BasketMiddleware',
-        ),
-        TEMPLATE_CONTEXT_PROCESSORS = (
-            "django.contrib.auth.context_processors.auth",
-            "django.core.context_processors.request",
-            "django.core.context_processors.debug",
-            "django.core.context_processors.i18n",
-            "django.core.context_processors.media",
-            "django.core.context_processors.static",
-            "django.contrib.messages.context_processors.messages",
-            # Oscar specific
-            'oscar.apps.search.context_processors.search_form',
-            'oscar.apps.promotions.context_processors.promotions',
-            'oscar.apps.checkout.context_processors.checkout',
-            'oscar.core.context_processors.metadata',
-            'oscar.apps.customer.notifications.context_processors.notifications',
-        ),
-        DEBUG=False,
-        HAYSTACK_CONNECTIONS={
-            'default': {
-                'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
-            },
-        },
-        TEMPLATE_DIRS=(OSCAR_MAIN_TEMPLATE_DIR,),
-        SITE_ID=1,
-        ROOT_URLCONF='tests.urls',
-        COMPRESS_ENABLED=False,
-        STATIC_URL='/',
-        STATIC_ROOT='/static/',
-        NOSE_ARGS=['-s', '--with-spec'],
-        # Oscar 1.0 factories assume this setting is present. Fixed in 1.1.
-        OSCAR_INITIAL_ORDER_STATUS='A',
-        **extra_settings
-    )
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_settings")
 
 from django_nose import NoseTestSuiteRunner
-
 
 def run_tests(*test_args):
     if not test_args:
