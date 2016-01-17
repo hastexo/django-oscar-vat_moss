9415e6b567f1c7e04eb6a97702c40ae4f783db29
Florian Haas <florian@hastexo.com>
2016-01-17 23:19:17 +0100

    Add VAT-enabled PaymentDetailsView

--- oscar_vat_moss/checkout/views.py
+++ oscar_vat_moss/checkout/views.py
@@ -1,5 +1,26 @@
+from oscar.apps.checkout.views import PaymentDetailsView as CorePaymentDetailsView  # noqa
+from django.contrib import messages
+
+from oscar_vat_moss import vat
+
 from . import assert_required_fields_set_correctly
 
 # Make sure OSCAR_REQUIRED_ADDRESS_FIELDS is set correctly for VAT
 # assessment
 assert_required_fields_set_correctly()
+
+
+class PaymentDetailsView(CorePaymentDetailsView):
+
+    def build_submission(self, **kwargs):
+        submission = super(PaymentDetailsView, self).build_submission(**kwargs)
+        try:
+            vat.apply_to(submission)
+            # Recalculate order total to ensure we have a
+            # tax-inclusive total
+            submission['order_total'] = self.get_order_totals(
+                submission['basket'], submission['shipping_charge'])
+        except vat.VATAssessmentException as e:
+            message = str(e)
+            messages.error(self.request, message)
+        return submission
