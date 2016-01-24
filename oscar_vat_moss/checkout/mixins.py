ef32711fd0166e066ebecdf9039e7ee6213dda03
Florian Haas <florian@hastexo.com>
2016-01-25 00:21:02 +0100

    Remove FixedRateVATCheckoutSessionMixin
    Not used anywhere, and ill-advised to begin with.

--- oscar_vat_moss/checkout/mixins.py
+++ oscar_vat_moss/checkout/mixins.py
@@ -8,10 +8,10 @@ from . import assert_required_fields_set_correctly
 assert_required_fields_set_correctly()
 
 
-class VATCheckoutSessionMixin(session.CheckoutSessionMixin):
+class DeferredVATCheckoutSessionMixin(session.CheckoutSessionMixin):
 
     def get_context_data(self, **kwargs):
-        ctx = super(VATCheckoutSessionMixin,
+        ctx = super(DeferredVATCheckoutSessionMixin,
                     self).get_context_data(**kwargs)
 
         # Oscar's checkout templates look for this variable which specifies to
@@ -19,36 +19,6 @@ class VATCheckoutSessionMixin(session.CheckoutSessionMixin):
         ctx['show_tax_separately'] = True
 
         return ctx
-
-
-class FixedRateVATCheckoutSessionMixin(VATCheckoutSessionMixin):
-
-    def check_a_valid_shipping_address_is_captured(self):
-        """Check that the just-captured shipping address is valid.
-
-        Normally, this method is only used to perform a general
-        shipping address check. With the FixedRateVATStrategy however,
-        we rely on the default shipping address to figure out a user's
-        applicable VAT rate. Therefore, when we are satisfied that the
-        shipping address is valid, we also set it as the default for
-        shipping. This will cause a minor nuisance for any user who
-        doesn't expect that the default shipping address is simply the
-        one used most recently, but in this case, the benefits
-        outweigh the disadvantages.
-
-        """
-        # The superclass does the VAT validation check. Once that has
-        # completed, we can set the shipping address to be the new
-        # default.
-        super(FixedRateVATCheckoutSessionMixin, self)
-
-        shipping_address = self.get_shipping_address(
-            basket=self.request.basket)
-        shipping_address.is_default_for_shipping = True
-        shipping_address.save()
-
-
-class DeferredVATCheckoutSessionMixin(VATCheckoutSessionMixin):
 
     def build_submission(self, **kwargs):
         submission = super(DeferredVATCheckoutSessionMixin,
@@ -67,8 +37,3 @@ class DeferredVATCheckoutSessionMixin(VATCheckoutSessionMixin):
             except vat.VATAssessmentException:
                 pass
         return submission
-
-
-# FixedRateVATCheckoutSessionMixin is the safer option,
-# make that the default
-CheckoutSessionMixin = FixedRateVATCheckoutSessionMixin
